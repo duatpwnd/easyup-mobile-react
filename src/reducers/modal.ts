81a8341aab4e5a15f37a39a5621be24ee61f0c06
duatpwnd <duatpwnd1@naver.com>
2021-08-05 12:25:39 +0900

    message modal  reducer and create action add

---
+++ src/reducers/modal.ts
@@ -0,0 +1,30 @@
+import * as toggle from "src/action/modal";
+import { createReducer, ActionType } from "typesafe-actions";
+interface StateType {
+  guideMessage?: string;
+  loginModal?: boolean;
+  guideMsgModal?: boolean;
+  mask: boolean;
+}
+const initialState: StateType = {
+  guideMessage: "",
+  guideMsgModal: false,
+  loginModal: false,
+  mask: false,
+};
+export const modalState = createReducer(initialState, {
+  [toggle.LOGIN_MODAL]: (state) => ({
+    loginModal: !state.loginModal,
+    mask: !state.mask,
+  }),
+  [toggle.GUIDE_MESSAGE_MODAL]: (state, action) => ({
+    guideMsgModal: true,
+    guideMessage: action.payload.message,
+    mask: true,
+  }),
+  [toggle.MASK]: (state, action) => {
+    return {
+      mask: action.payload.mask,
+    };
+  },
+});
