23bdc2451a7526858d0703aea591f6bcc1cb7fc9
duatpwnd <duatpwnd1@naver.com>
2021-08-05 12:31:22 +0900

    새로고침시 쿠키에 유저정보 있을경우 store에 유저정보 셋팅

--- src/App.tsx
+++ src/App.tsx
@@ -3,24 +3,36 @@ import 'src/assets/scss/common.scss'
 import Header from "src/components/layout/Header"
 import Footer from "src/components/layout/Footer"
 import LoginModal from "src/components/login/LoginModal"
+import { useEffect } from 'react';
 import { useSelector, useDispatch } from "react-redux";
-import Store from "src/reducers/index"
+import { useCookies } from 'react-cookie';
 import { Route } from "react-router-dom";
+import Store from "src/reducers/index"
 import SignUp from './views/SignUp';
 import Main from './views/Main';
 import CsCenter from "./views/CsCenter"
 import MyClass from "./views/MyClass"
 import GuideMsgModal from "src/components/common/GuideMsgModal"
-import * as toggle from "src/action/toggle"
+import * as toggle from "src/action/modal"
+import * as user from "src/action/userInfo"
 
 const App = () => {
   const { loginModal, mask, guideMsgModal } = useSelector((state: ReturnType<typeof Store>) => {
-    return state.toggleState
+    return state.modalState
   })
   const dispatch = useDispatch();
   const maskToggle = () => {
     dispatch(toggle.maskModal({ mask: false }))
   }
+  const [cookies, setCookie] = useCookies();
+  useEffect(() => {
+    if (cookies.user_info != null) {
+      console.log('유저정보있습니다.');
+      dispatch(user.userInfoSet({
+        userInfo: cookies.user_info.info
+      }))
+    }
+  })
   return (
     <div className="app">
       <Header></Header>
