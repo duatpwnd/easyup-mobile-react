d7f0ac53eb22a52210a1f82388e23718f496b762
duatpwnd <duatpwnd1@naver.com>
2021-08-06 19:05:02 +0900

    강의리스트 컴포넌트 추가

---
+++ src/views/LectureList.tsx
@@ -0,0 +1,11 @@
+// import "./LectureList.scoped.scss"
+const LectureList = (props: { [key: string]: any }) => {
+    console.log(props.location.state);
+    return (
+        <div>
+            카테고리 리스트
+        </div>
+    )
+
+}
+export default LectureList
