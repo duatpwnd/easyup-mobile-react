bb672a169e603e7a944f498911f0a43478e1603a
duatpwnd <duatpwnd1@naver.com>
2021-08-06 19:05:23 +0900

    강의상세 api연동"

--- src/views/LectureDetail.tsx
+++ src/views/LectureDetail.tsx
@@ -12,7 +12,6 @@ const LectureDetail = ({ location }) => {
         course_id: query.id
     }
     useEffect(() => {
-        console.log('useEffect');
         axios
             .post("", JSON.stringify(body))
             .then((result) => {
@@ -20,41 +19,52 @@ const LectureDetail = ({ location }) => {
                 setDetail(result.data.data)
             });
     }, [])
-    return (
-        <div>
-            <img
-                alt="파이썬 코딩 기본편"
-                title="파이썬 코딩 기본편"
-                src={detail.course_image}
-            />
-            <div className="update-noti">
-                <span
-                >{detail.creation_date}(업데이트 일자:{
-                        detail.update_date
-                    })</span
-                >
-            </div>
-            <section className="section1">
-                <div className="lecture-title">
-                    <h3 className="subtitle" >{detail.category}</h3>
-                    <h2 className="title">{detail.title}</h2>
-                </div>
-                <div className="star-rating">
-                    <Ratings
-                        rating={detail.ranking}
-                        widgetDimensions="30px"
-                        widgetSpacings="2px"
-                        widgetEmptyColors="#ccc"
+    if (Object.keys(detail).length > 0) {
+        return (
+            <div>
+                <img
+                    alt="파이썬 코딩 기본편"
+                    title="파이썬 코딩 기본편"
+                    src={detail.course_image}
+                />
+                <div className="update-noti">
+                    <span
+                    >{detail.creation_date}(업데이트 일자:{
+                            detail.update_date
+                        })</span
                     >
-                        <Ratings.Widget widgetRatedColor="gold" />
-                        <Ratings.Widget widgetRatedColor="gold" />
-                        <Ratings.Widget widgetRatedColor="gold" />
-                        <Ratings.Widget widgetRatedColor="gold" />
-                        <Ratings.Widget widgetRatedColor="gold" />
-                    </Ratings>
                 </div>
-            </section>
-        </div >
-    )
+                <section className="section1">
+                    <div className="lecture-title">
+                        <h3 className="subtitle" >{detail.category}</h3>
+                        <h2 className="title">{detail.title}</h2>
+                    </div>
+                    <div className="star-rating">
+                        <Ratings
+                            rating={detail.ranking}
+                            widgetDimensions="30px"
+                            widgetSpacings="2px"
+                            widgetEmptyColors="#ccc"
+                        >
+                            <Ratings.Widget widgetRatedColor="gold" />
+                            <Ratings.Widget widgetRatedColor="gold" />
+                            <Ratings.Widget widgetRatedColor="gold" />
+                            <Ratings.Widget widgetRatedColor="gold" />
+                            <Ratings.Widget widgetRatedColor="gold" />
+                        </Ratings>
+                    </div>
+                    {detail.price.is_free ? <h2 className="free">무료</h2> :
+                        <div className="price">
+                            {detail.price.format_original != detail.price.format_final ? <del className="original">{detail.price.format_original}</del> : ""}
+                            <span className="final">{detail.price.format_final}원</span>
+                        </div>
+                    }
+                </section>
+            </div >
+        )
+    } else {
+        return null
+    }
+
 }
 export default LectureDetail
