32f2f9bb23255b0741cbd0b6fd9ba10ef44c9e54
duatpwnd <duatpwnd1@naver.com>
2021-08-10 14:35:46 +0900

    검색창 컴포넌트 로직수정

--- src/components/common/BaseSearchInput.tsx
+++ src/components/common/BaseSearchInput.tsx
@@ -3,8 +3,13 @@ import "./BaseSearchInput.scoped.scss"
 const BaseSearchInput = (props: { [key: string]: any }) => {
     return <div className="search">
         {props.left}
-        <input type="text" className="search-input" placeholder={props.placeholder} />
-        {props.right}
+        <input type="text" className="search-input" onChange={(e) => {
+            props.keywordEvent(e.currentTarget.value)
+        }} placeholder={props.placeholder} />
+        <button
+            className="search-button"
+            onClick={props.buttonEvent}
+        ></button>
     </div>
 
 }
