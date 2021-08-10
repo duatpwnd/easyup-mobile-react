0989c320d74b31cf70b5dd020951b4ffbe774556
duatpwnd <duatpwnd1@naver.com>
2021-08-10 14:33:43 +0900

    페이징컴포넌트 로직 변경

--- src/components/common/Pagination.tsx
+++ src/components/common/Pagination.tsx
@@ -1,18 +1,18 @@
 
 import "./Pagination.scoped.scss"
 import { Link } from "react-router-dom";
-const Pagination = ({ to, current, total }) => {
+const Pagination = ({ to, total }) => {
+    console.log(to);
     return (<ul className="paging">
-        {current > 1 ? <li className="prev">
-            <Link to={to + (Number(current) - 1)}>이전페이지</Link>
+        {to.pageCurrent > 1 ? <li className="prev">
+            <Link to={{ state: { ...to, pageCurrent: Number(to.pageCurrent) - 1 } }}>이전페이지</Link>
         </li> : ""}
-        {total != current && total > 1 ?
+        {total != to.pageCurrent && total > 1 ?
             <li className="next">
-                <Link to={to + (Number(current) + 1)}>다음페이지</Link>
+                <Link to={{ state: { ...to, pageCurrent: Number(to.pageCurrent) + 1 } }}>다음페이지</Link>
             </li>
             : ""}
     </ul>)
 
 }
-// ${Number(match.params.paging) + 1}
 export default Pagination
