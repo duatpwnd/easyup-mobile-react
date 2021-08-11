d095b8f624cc2c3af7d99dd16f06fc6873138c7d
duatpwnd <duatpwnd1@naver.com>
2021-08-11 12:17:38 +0900

    강의상세페이지 커리큘럼 리스트 토글이벤트 추가

--- src/views/LectureDetail.tsx
+++ src/views/LectureDetail.tsx
@@ -1,7 +1,7 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useMemo } from 'react';
 import { Link } from "react-router-dom";
 import axios from 'axios';
-import "./LectureDetail.scss"
+import "./LectureDetail.scoped.scss"
 import Ratings from 'react-ratings-declarative';
 const LectureDetail = ({ location }) => {
     const [detail, setDetail] = useState<{ [key: string]: any }>({})
@@ -9,15 +9,55 @@ const LectureDetail = ({ location }) => {
         action: "get_course_info",
         course_id: location.state.id
     }
+    const CurrList = ({ li }) => {
+        const [isActive, toggle] = useState(false);
+        return (
+            <div key={li.title} onClick={() => { toggle(!isActive) }}>
+                <span className={`full-title ${li.children_count == null ? "" : "not-full-title"}`}>
+                    <span className="lec-title-section" dangerouslySetInnerHTML={{ __html: li.title }}></span>
+                    {() => {
+                        if (li.up_status == 1 && detail.isApprove == 0) {
+                            <span className="ing-ico">변환중</span>
+                        } else if (li.up_status == 2 && detail.isApprove == 0) {
+                            <span className="complete-ico">완료</span>
+                        }
+                    }}
+                </span>
+                {li.children_count != null && <span className="lec_num"> {li.children_count}개</span>}
+                {/* 커리큘럼 하위리스트 */}
+                {detail.curriculum_list.type == "section" &&
+                    isActive && <div className="child-list">
+                        {li.children_list.map((child) =>
+                            <div
+                                className="child-list-title"
+                                key={child.title}
+                            >
+                                <span className="child-title-section" dangerouslySetInnerHTML={{ __html: child.title }}></span>
+                                <span
+                                    className="ing-ico"
+                                >변환중</span
+                                >
+                                <span
+                                    className="complete-ico"
+                                >완료</span
+                                >
+                            </div>
+                        )}
+                    </div>
+                }
+            </div>
+        )
+    }
     useEffect(() => {
         axios
             .post("", JSON.stringify(body))
             .then((result) => {
                 console.log(result);
                 setDetail(result.data.data)
+
             });
     }, [])
-    if (Object.keys(detail).length > 0) {
+    if (detail.price != undefined) {
         return (
             <div>
                 <img
@@ -57,8 +97,73 @@ const LectureDetail = ({ location }) => {
                             <span className="final">{detail.price.format_final}원</span>
                         </div>
                     }
+                    <div id="tag_wrap">
+                        <div className="tag_list">
+                            {detail.tags.map((li, index) =>
+                                <Link
+                                    className="tag"
+                                    key={index}
+                                    to={{
+                                        pathname: '/category',
+                                        state: {
+                                            action: 'get_course_list',
+                                            pageCurrent: 1,
+                                            order: 'type_date',
+                                            keyword: '',
+                                            tag: li.tag.replace('#', ''),
+                                        },
+                                    }}
+                                >
+                                    {li.tag}</Link
+                                >
+                            )}
+                        </div>
+                    </div>
                 </section>
-            </div >
+                <section className="section2">
+                    <div className="user_intro">
+                        <div className="teacher-profile-info">
+                            <span className="name teacher-profile" style={{
+                                background: `url(${detail.teachers.profile_image}) no-repeat center / 100% 100%`
+                            }}></span>
+                            <span className="teacher-name">{detail.teachers.name}</span>
+                        </div>
+                        <div>
+                            <span className="access_limit">{detail.access_limit.basic}</span>
+                        </div>
+                        <div>
+                            <span className="total_lec"
+                            ><span className="color">
+                                    {detail.curriculum_list.count_detail}</span
+                                >개 레슨</span
+                            >
+                        </div>
+                    </div>
+                </section>
+                <div id="intro">
+                    {detail.description.map((li, index) =>
+                        <div className="example" key={index}>
+                            {li.underline ? <h2 className="title" dangerouslySetInnerHTML={{ __html: li.title }}></h2>
+                                : <h2 dangerouslySetInnerHTML={{ __html: li.title }}></h2>}
+                            <div className="description_contents" dangerouslySetInnerHTML={{ __html: li.content }}></div>
+                        </div>
+                    )}
+                </div>
+                {/* 커리큘럼 */}
+                <div className="curriculum">
+                    <div className="curriculum_header">
+                        <h2 className="curriculum_title">커리큘럼</h2>
+                        <span className="total_lec"
+                        >{detail.curriculum_list.count_detail}개 레슨</span
+                        >
+                    </div>
+                    <div className="curriculum_list">
+                        {detail.curriculum_list.items.map((li, index) =>
+                            <CurrList li={li} key={index}></CurrList>
+                        )}
+                    </div>
+                </div>
+            </div>
         )
     } else {
         return null
