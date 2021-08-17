c24fc94df2d67b568745c1f30d8fc168914ce06f
duatpwnd <duatpwnd1@naver.com>
2021-08-17 15:35:47 +0900

    구독버튼 색상 추가

--- src/components/common/BaseButton.tsx
+++ src/components/common/BaseButton.tsx
@@ -2,7 +2,7 @@
 import "./BaseButton.scoped.scss"
 const BaseButton = (props: { [key: string]: any }) => {
     console.log(props);
-    return (<button type="button" className="base-button" onClick={props.handleClick}>{props.name}</button>)
+    return (<button type="button" className={`base-button ${props.className}`} onClick={props.handleClick}>{props.name}</button>)
 
 }
 export default BaseButton
