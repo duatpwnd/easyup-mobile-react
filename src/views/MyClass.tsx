44c8493be3cb260a2cb1ffd5979c3ddec869df70
duatpwnd <duatpwnd1@naver.com>
2021-08-25 17:50:14 +0900

    내강의실 유저 프로필이미지 추가

--- src/views/MyClass.tsx
+++ src/views/MyClass.tsx
@@ -1,8 +1,44 @@
 import MyClassGnb from "src/components/gnb/MyClassGnb"
-const MyClass = () => {
+import Store from "src/reducers/index"
+import "./MyClass.scoped.scss"
+import { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+const UserProfile = ({ props }) => {
+    const state = props.location.state;
+    const dispatch = useDispatch();
+    const userInfo = useSelector((state: ReturnType<typeof Store>) => {
+        console.log(state.userInfoSet.userInfo);
+        return (state.userInfoSet.userInfo) as { [key: string]: any }
+    })
+    useEffect(() => {
+    }, [])
+    return (
+        userInfo != null ? <div>
+            <span
+                className="profile"
+                style={{
+                    background: `url(${userInfo.profile_image}) no-repeat 
+      center / 100% 100%`,
+                }}
+            >
+            </span>
+            <span className="name">{userInfo.username}님의 강의실</span>
+            <p className="email">
+                {userInfo.email}
+            </p>
+            {state != undefined || userInfo.status == 1 && <div>
+                <span className="report">프로필</span>
+                <span className="convert">학생전환</span>
+                <span className="convert">강사전환</span>
+            </div>}
+        </div> : null
+    )
+};
+const MyClass = (props) => {
+    console.log(props);
     return (
         <div>
-            내강의실
+            <UserProfile props={props}></UserProfile>
             <MyClassGnb></MyClassGnb>
         </div>
     )
