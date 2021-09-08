13a84c35e132b4732940cc082089734a0fc29bb0
duatpwnd <duatpwnd1@naver.com>
2021-09-08 18:23:57 +0900

    footer에 이용약관, 개인정보취급방침, 고객센터 추기

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,5 +1,5 @@
 import FooterLogo from 'src/assets/images/main/footer_logo.png'
-import { withRouter } from "react-router-dom";
+import { withRouter, Link } from "react-router-dom";
 import "./Footer.scoped.scss"
 const Footer = withRouter(({ match, location }) => {
     const arr = ["/signUp", "/signUpComplete", "/play"]
@@ -15,10 +15,11 @@ const Footer = withRouter(({ match, location }) => {
                 title="EASYUP"
             />
             <div className="gnb">
+                <Link to="/policy">이용약관</Link>
                 <span className="bar">|</span>
-
+                <Link to="/policy">개인정보취급방침</Link>
                 <span className="bar">|</span>
-
+                <Link to="/help/notice">고객센터</Link>
             </div>
             <address>
                 <pre>(주)유니윌<br />대표이사:조인형   주소:서울시 강남구 테헤란로124<br />사업자 등록번호:220-86-20171    통신판매업신고:강남 제3339호</pre>
